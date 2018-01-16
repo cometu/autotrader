9daea501b9dcf7ca9e82e05b32a11918b62205fe
cometu <come.on.ct@gmail.com>
2018-01-16 18:56:42 +0100

    Fix bugs.

--- autotrader.py
+++ autotrader.py
@@ -16,11 +16,13 @@ import matplotlib.pyplot as plt
 import random
 from matplotlib import cm
 
-def fetchKrakenOhlc(pair, interval, since=0):
-    data1 = k.query_public('OHLC', {'pair': pair, 'interval': interval, 'since': since})
+
+def fetchKrakenOhlc(krakenApi, pair, interval, since=0):
+    data1 = krakenApi.query_public('OHLC', {'pair': pair, 'interval': interval, 'since': since})
     data = data1['result'][pair]
     df = pd.DataFrame(data)
     df.columns = ['time', 'open', 'high', 'low', 'close', 'vwap', 'volume', 'count']
+    df = df.apply(pd.to_numeric)
     return df
 
 
@@ -31,17 +33,17 @@ def estimateLongProfits(data, signals, fee):
     stepInvestment = 100
 
     for i in range(len(signals)):
-        if signals.action.iloc[i] == 'buy':
+        if signals.action.iloc[i] == 2:
             # Stop loop if it's the last buy
             if i == len(signals)-1:
                 break
-
-            soldEth += stepInvestment/data[data.time == signals.time].value.iloc[0]
+            # print(data.value.loc[signals.index[i]])
+            soldEth += stepInvestment/data.value.loc[signals.index[i]]
             soldEur -= stepInvestment
             soldEur -= fee*stepInvestment
 
         else:
-            soldEur += (1-fee)*soldEth*data[data.time == signals.time].value.iloc[0]
+            soldEur += (1-fee)*soldEth*data.value.loc[signals.index[i]]
             soldEth = 0
 
     soldEur += soldEth*data.value.iloc[-1]
@@ -59,13 +61,13 @@ def estimateShortProfits(data, signals, fee):
     stepInvestment = 100
 
     for i in range(len(signals)):
-        if signals.action.iloc[i] == 'buy':
-            soldEur -= (1+fee)*debtBankEth*data[data.time == signals.time].value.iloc[0]
+        if signals.action.iloc[i] == 2:
+            soldEur -= (1+fee)*debtBankEth*data.value.loc[signals.index[i]]
             debtBankEth = 0
         else:  # sell
             if i == len(signals)-1:
                 break
-            debtBankEth += stepInvestment/data[data.time == signals.time].value.iloc[0]
+            debtBankEth += stepInvestment/data.value[signals.index[i]]
             soldEur += stepInvestment
             soldEur -= fee*stepInvestment
 
@@ -84,13 +86,10 @@ def estimateProfitsOnOhlcUsingEma(period1, period2, ohlcData, fee, trainOnOpen=T
         ema1 = pd.ewma(ohlcData.close, span=period1)
         ema2 = pd.ewma(ohlcData.close, span=period2)
 
-    signals = pd.DataFrame(columns=['action', 'time'])
+    signals = pd.DataFrame(columns=['action'], index=ohlcData.loc[np.where(np.diff(np.sign((ema1-ema2))))[0]].index)
+    signals.action = np.diff(np.sign((ema1-ema2)))[signals.index]
 
-    signals.time = np.where(np.diff(np.sign((ema1-ema2))))[0]
-    signals.action = np.diff(np.sign((ema1-ema2)))[signals.time]
-
-    data = pd.DataFrame(columns=['time', 'value'])
-    data.time = ohlcData.time
+    data = pd.DataFrame(columns=['value'])
 
     if testOnClose is False:
         data.value = ohlcData.close
@@ -102,14 +101,7 @@ def estimateProfitsOnOhlcUsingEma(period1, period2, ohlcData, fee, trainOnOpen=T
     profit += estimateShortProfits(data=data, signals=signals, fee=fee)
     return profit
 
-profit = 0
 result = pd.DataFrame(columns=['pair', 'interval', 'p1', 'p2', 'profit'])
-
-p1Min = 1
-p1Max = 50
-
-p2Min = 1
-p2Max = 50
 
 pair = 'XXBTZEUR'
 
@@ -117,14 +109,15 @@ pair = 'XXBTZEUR'
 def brutforceEma(ohlcData, fee=0.0026,
                  p1Min=1, p1Max=50, p2Min=1, p2Max=50,
                  trainOnOpen=True, testOnClose=True):
-    result = pd.DataFrame(columns=['pair', 'interval', 'p1', 'p2', 'profit'])
+    result = pd.DataFrame(columns=['p1', 'p2', 'profit'])
     for p2 in range(p2Min, p2Max):
         for p1 in range(p1Min, p1Max):
-            profit = estimateProfitsOnOhlcUsingEma(p1, p2, d, fee=fee).values[0]
+            if p1 == p2:
+                profit = 0
+            else:
+                profit = estimateProfitsOnOhlcUsingEma(p1, p2, ohlcData, fee=fee)
             result = result.append(
                 {
-                    'pair': pair,
-                    'interval': interval,
                     'p1': p1,
                     'p2': p2,
                     'profit': profit
@@ -133,53 +126,26 @@ def brutforceEma(ohlcData, fee=0.0026,
 
     return result
 
-interval = 5
-d = fetchKrakenOhlc(pair=pair, interval=interval, since=0)
-duration_5 = (d.time.iloc[-1]-d.time.iloc[0])/3600
-result = brutforceEma(d)
 
-interval = 15
-d = fetchKrakenOhlc(pair=pair, interval=interval, since=0)
-duration_15 = (d.time.iloc[-1]-d.time.iloc[0])/3600
-result = result.append(brutforceEma(d))
+# # Plot
+# def fun(p1, p2):
+#     ret = result[result.interval==5][(result[result.interval==5].p1 == p1) & (result[result.interval==5].p2 == p2)].profit.iloc[0]
+#     return ret
 
-interval = 30
-d = fetchKrakenOhlc(pair=pair, interval=interval, since=0)
-duration_30 = (d.time.iloc[-1]-d.time.iloc[0])/3600
-result = result.append(brutforceEma(d))
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
-interval = 60
-d = fetchKrakenOhlc(pair=pair, interval=interval, since=0)
-duration_60 = (d.time.iloc[-1]-d.time.iloc[0])/3600
-result = result.append(brutforceEma(d))
+# x = y = range(1,50)
+# X, Y = np.meshgrid(x, y)
+# zs = np.array([fun(x,y) for x,y in zip(np.ravel(X), np.ravel(Y))])
+# Z = zs.reshape(X.shape)
 
-# Compute per hour profit
-r = result.copy()
-r.profit_per_hour = r.apply(lambda x: x.profit/duration_60 if x.interval == 60 else x.profit, axis=1)
-r.profit_per_hour = r.apply(lambda x: x.profit/duration_15 if x.interval == 15 else x.profit, axis=1)
-r.profit_per_hour = r.apply(lambda x: x.profit/duration_30 if x.interval == 30 else x.profit, axis=1)
-r.profit_per_hour = r.apply(lambda x: x.profit/duration_5 if x.interval == 5 else x.profit, axis=1)
+# ax.plot_surface(X, Y, Z)
 
+# ax.set_xlabel('p1')
+# ax.set_ylabel('p2')
+# ax.set_zlabel('profit')
 
-# Plot
-def fun(p1, p2):
-    ret = result[result.interval==5][(result[result.interval==5].p1 == p1) & (result[result.interval==5].p2 == p2)].profit.iloc[0]
-    return ret
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-x = y = range(1,50)
-X, Y = np.meshgrid(x, y)
-zs = np.array([fun(x,y) for x,y in zip(np.ravel(X), np.ravel(Y))])
-Z = zs.reshape(X.shape)
-
-ax.plot_surface(X, Y, Z)
-
-ax.set_xlabel('p1')
-ax.set_ylabel('p2')
-ax.set_zlabel('profit')
-
-plt.show()
+# plt.show()
 
 
